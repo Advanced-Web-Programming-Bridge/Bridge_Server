e7b4bc863ee2670bf8e1dcd56b84bf317dc2c63b
namsh1125 <namsh1125@naver.com>
2023-05-17 00:15:10 +0900

    Change Dto (remove pw)

--- user-service/src/main/java/gachon/bridge/userservice/dto/UserDto.java
+++ user-service/src/main/java/gachon/bridge/userservice/dto/UserDto.java
@@ -6,12 +6,10 @@ import lombok.Data;
 @Data
 public class UserDto {
     private String userId;
-    private String pw;
     private String email;
 
     public UserDto(User user) {
         this.userId = user.getUserId();
-        this.pw = user.getPw();
         this.email = user.getEmail();
     }
 }
