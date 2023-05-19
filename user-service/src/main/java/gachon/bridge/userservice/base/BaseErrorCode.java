780a8ea881015d1573f05cf30125d59de68df1e2
namsh1125 <namsh1125@naver.com>
2023-05-19 15:25:16 +0900

    Update status codes in API responses

--- user-service/src/main/java/gachon/bridge/userservice/base/BaseErrorCode.java
+++ user-service/src/main/java/gachon/bridge/userservice/base/BaseErrorCode.java
@@ -6,47 +6,36 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum BaseErrorCode {
+    // 400 Bad request
+    INVALID_UUID_FORMAT(400, "올바르지 않은 UUID 형식입니다."),
+    ID_TOO_SHORT(400, "유저 아이디가 너무 짧습니다"),
+    ID_TOO_LONG(400, "유저 아이디가 너무 깁니다."),
 
-    // 암호화, 복호화 에러
+    PW_TOO_SHORT(400, "비밀번호가 너무 짧습니다."),
+    PW_TOO_LONG(400, "비밀번호가 너무 깁니다."),
+    INVALID_PW(400, "비밀번호가 일치 하지 않습니다."),
+    INVALID_INFORMATION(400, "올바르지 않은 정보입니다."),
+    INVALID_EMAIL(400, "유효하지 않는 이메일입니다."),
+
+    // 401 Unauthorized
+    CORRUPTED_TOKEN(401, "유효하지 않은 토큰입니다."),
+    EXPIRED_TOKEN(401, "만료된 토큰입니다."),
+    UNSUPPORTED_TOKEN(401, "지원하지 않는 토큰입니다."),
+    TOKEN_NOT_EXIST(401, "토큰이 존재하지 않습니다."),
+
+    // 403 Forbidden
+    INVALID_JWT_TOKEN(403, "권한이 없는 토큰입니다."),
+
+    // 404 Not Found
+    INVALID_USER(404, "존재하지 않는 회원입니다."),
+
+    // 409 Conflict
+    EXIST_ID(409, "이미 존재하는 아이디 입니다."),
+
+    // 500 Internal Server Error
     ENCRYPTION_ERROR(500, "암호화하는 과정에서 에러가 발생하였습니다."),
     DECRYPTION_ERROR(500, "복호화하는 과정에서 에러가 발생하였습니다."),
-
-    /**
-     * 1000 ~ 1500
-     * Auth Token Error
-     */
-    INVALID_JWT_TOKEN(1000, "권한이 없는 토큰입니다."),
-    CORRUPTED_TOKEN(1001, "유효하지않은 토큰입니다."),
-    EXPIRED_TOKEN(1002, "만료된 토큰입니다."),
-    UNSUPPORTED_TOKEN(1003, "지원하지 않는 토큰입니다."),
-    TOKEN_NOT_EXIST(1004, "토큰이 존재하지 않습니다."),
-
-    /**
-     * 1501 ~ 1999
-     * Auth Controller Error
-     */
-
-    // Id
-    ID_TOO_SHORT(1500, "유저 아이디가 너무 짧습니다"),
-    ID_TOO_LONG(1501, "유저 아이디가 너무 깁니다."),
-    EXIST_ID(1502, "이미 존재하는 아이디 입니다."),
-
-    // Password
-    PW_TOO_SHORT(1600, "비밀번호가 너무 짧습니다."),
-    PW_TOO_LONG(1601, "비밀번호가 너무 깁니다."),
-    INVALID_PW(1602, "비밀번호가 일치 하지 않습니다."),
-
-    // User
-    INVALID_USER(1700, "존재하지 않는 회원입니다."),
-    INVALID_INFORMATION(1701, "올바르지 않은 정보입니다."),
-
-    // Email
-    INVALID_EMAIL(1800, "유효하지 않는 이메일입니다."),
-
-    /**
-     * 9000 Database Error
-     */
-    DATABASE_ERROR(9000, "Database Error");
+    DATABASE_ERROR(500, "Database Error");
 
     private final Integer status;
     private final String message;
